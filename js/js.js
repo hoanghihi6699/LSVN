f3bf716c7dbd59340e34c2ab321327006d7b4d8a
hoanghihi6699 <trananhhoang6699@gmail.com>
2020-04-29 15:05:41 +0700

    second

--- js/js.js
+++ js/js.js
@@ -1,5 +1,35 @@
 
-var x = 0;
-function navScroll() {
-  document.getElementById("demo").innerHTML = x += 1;
+// Maxtext
+$(".label_title").each(function () {
+    if ($(this).text().length > 170) {
+        $(this).text($(this).text().substr(0, 170));
+        $(this).append('...');
+    }
+});
+// Timeline
+
+$.js = function (el) {
+    return $('[data-js=' + el + ']')
+};
+
+function carousel() {
+    $.js('timeline-carousel').slick({
+        infinite: true ,
+        arrows: false,
+        dots: true,
+        autoplay: false,
+        speed: 100,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+    });
 }
+
+carousel();
